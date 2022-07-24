7d84839c6bd889a42a104713358f067e8f873bc3
Pranav Prabhu <59041083+pranavprabhu01@users.noreply.github.com>
2022-07-24 18:56:37 +0530

    Add files via upload
    testbench with failure case

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -2,9 +2,19 @@
 
 import cocotb
 from cocotb.triggers import Timer
+import random
 
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    A=2
+    dut.sel.value=30
+    dut.inp30.value=A
+    await Timer(2, units='ns')
+    dut._log.info(f'DUT={int(dut.out.value):02}')
+    assert dut.out.value==A
+     
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+
+
+    
